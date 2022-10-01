f5f70aa81dbb0a2a14137cc279f2df0151e45cd5
Roberto Ramirez <robert.624@hotmail.com>
2022-10-01 16:40:16 -0500

    added handler to find by id and update its fields

--- myApp.js
+++ myApp.js
@@ -113,9 +113,18 @@ const findPersonById = (personId, done) => {
 };
 
 const findEditThenSave = (personId, done) => {
+  
   const foodToAdd = "hamburger";
+  Person.findById(personId, function(err, person){
+    if(err) return console.log(err)
 
-  done(null /*, data*/);
+    person.favoriteFoods.push(foodToAdd)
+
+    person.save((err, updatedPerson)=>{
+      if(err) return console.log(err)
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
