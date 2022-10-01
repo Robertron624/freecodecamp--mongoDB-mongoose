98471ede2c9e171c038d207735f910578d6bf454
Roberto Ramirez <robert.624@hotmail.com>
2022-10-01 00:17:51 -0500

    added mongodb database connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
 
+let mongoose
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+
+try{
+  mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+  console.log('Conectado correctamente a la BD')
+}catch(e){
+  console.error('ERROR al tratar de conectarse a la BD')
+}
 
 let Person;
 
